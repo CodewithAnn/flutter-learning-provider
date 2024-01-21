c500abfeecde52d671ffe972510363ed07aaefce
Anand jha <codewithann@gmail.com>
2024-01-22 01:08:03 +0530

    create counter App
        - learned how to use Provider(single Provider)

--- flutter_provider/lib/Provider/counter_provider.dart
+++ flutter_provider/lib/Provider/counter_provider.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
-class CounterProvider with ChangeNotifier{
-  
+class CounterProvider with ChangeNotifier {
+  int _count = 0;
+  int get count => _count;
+
+  void setCount(){
+    _count++;
+    notifyListeners();
+  }
 }
--- flutter_provider/lib/Screens/counter-App/counterApp.dart
+++ flutter_provider/lib/Screens/counter-App/counterApp.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider/Provider/counter_provider.dart';
+import 'package:provider/provider.dart';
 
 class CounterApp extends StatefulWidget {
   const CounterApp({super.key});
@@ -10,15 +12,26 @@ class CounterApp extends StatefulWidget {
 class _CounterAppState extends State<CounterApp> {
   @override
   Widget build(BuildContext context) {
+    final counterProvider =
+        Provider.of<CounterProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text("Counter App"),
       ),
       body: Center(
-        child: Text("0"),
+        child: Consumer<CounterProvider>(
+          builder: (context, value, child) {
+            return Text(
+              value.count.toString(),
+              style: TextStyle(fontSize: 50),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          counterProvider.setCount();
+        },
         child: Icon(Icons.add),
       ),
     );
