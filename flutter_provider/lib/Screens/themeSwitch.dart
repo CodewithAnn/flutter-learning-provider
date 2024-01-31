bed70e3ec8eff3c1be164b9288e33d5af03771ad
Anand jha <codewithann@gmail.com>
2024-01-31 22:10:13 +0530

    added a package, switch and container
        - package name is flutter_svg and flutter_switch
        - container added to spourt the UI
        - flutter_svg added to use svg file
        - flutter_switch added to use the switch to toggle theme
        TODO: implement the provider now in UI

--- flutter_provider/lib/Screens/themeSwitch.dart
+++ flutter_provider/lib/Screens/themeSwitch.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 class ThemeSwitch extends StatefulWidget {
   const ThemeSwitch({super.key});
@@ -8,8 +10,56 @@ class ThemeSwitch extends StatefulWidget {
 }
 
 class _ThemeSwitchState extends State<ThemeSwitch> {
+  bool isDark = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Theme"),
+        actions: [
+          FlutterSwitch(
+            value: isDark,
+            onToggle: (value) {},
+            activeIcon: SvgPicture.asset("assets/images/moon.svg"),
+            inactiveIcon: SvgPicture.asset("assets/images/sun.svg"),
+            toggleSize: 30,
+            height: 40,
+            toggleColor: Colors.yellow,
+            inactiveColor: Colors.yellow.shade200,
+          ),
+          const SizedBox(
+            width: 10,
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.red,
+              height: 100,
+              width: 100,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.yellow.shade900,
+              height: 100,
+              width: 100,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.green.shade600,
+              height: 100,
+              width: 100,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
