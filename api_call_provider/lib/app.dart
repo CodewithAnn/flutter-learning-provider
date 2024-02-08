2ba480671a5bd86c32ccc15c9126e84c404ea466
Anand jha <codewithann@gmail.com>
2024-02-08 19:37:59 +0530

    added folder and packages

---
+++ api_call_provider/lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:api_call_provider/screens/home_Screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
