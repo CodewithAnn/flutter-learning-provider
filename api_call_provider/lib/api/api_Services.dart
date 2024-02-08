bebe75c812a4bb0f8925a5b51eba5c6ea4e6e0d2
Anand jha <codewithann@gmail.com>
2024-02-08 20:37:52 +0530

    call api
        - api called
        - TODO: FETCH IT in screen and write Provider

--- api_call_provider/lib/api/api_Services.dart
+++ api_call_provider/lib/api/api_Services.dart
@@ -1,3 +1,21 @@
+import 'dart:convert';
+
+import 'package:api_call_provider/models/dummy_api.dart';
 import 'package:http/http.dart' as http;
 
-class ApiServices {}
+class ApiServices {
+  Future<DummyApi> getDummyApi() async {
+    final uri = Uri.parse("https://reqres.in/api/users?page=2");
+    final response = await http.get(uri);
+    late DummyApi data;
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      data = DummyApi.fromJson(jsonDecode(response.body));
+    } else {
+      print(
+        "Http error on getting dummy api, status code: ${response.statusCode}",
+      );
+    }
+    return data;
+  }
+}
