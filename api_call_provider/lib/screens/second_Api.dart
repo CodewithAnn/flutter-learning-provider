35f005ba3a05848dfc460503ceb00bfdcf070daf
Anand jha <codewithann@gmail.com>
2024-02-10 22:57:34 +0530

    fetched data
        - Data fetched without any model

--- api_call_provider/lib/screens/second_Api.dart
+++ api_call_provider/lib/screens/second_Api.dart
@@ -1,3 +1,4 @@
+import 'package:api_call_provider/Provider/user_api_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,16 +13,39 @@ class _SecondApiState extends State<SecondApi> {
   @override
   void initState() {
     // TODO: implement initState
-    
-    Provider.of<>(context, listen: false).();
+
+    Provider.of<UserApiProvider>(context, listen: false).userData();
   }
+
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<UserApiProvider>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue.shade300,
         title: Text("Api Call"),
       ),
+      body: provider.loading
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: ListView.builder(
+                  itemCount: provider.model[''].toString().length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(provider.model['image'].toString()),
+                      ),
+                      title: Text(provider.model['firstName'].toString()),
+                      trailing: Text(provider.model['phone'].toString()),
+                    );
+                  },
+                ))
+              ],
+            ),
     );
   }
 }
